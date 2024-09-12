83534b74595c3483424ded04f4a5b31813981a25
Oleinikov <j-n-d@yandex.ru>
2024-09-12 14:30:22 +0300

    add constants file, add Ingredient model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+from foodgram_backend import constants
+
+
+class Ingredient(models.Model):
+    name = models.CharField('Название', max_length=constants.NAME_MAX_LENGTH)
+    measurement_unit = models.CharField('Единица измерения')
+

69664462909277fa1d258ae6858fd7f129b524b0
Oleinikov <j-n-d@yandex.ru>
2024-10-06 20:10:09 +0300

    updates

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py run_import
+python manage.py collectstatic --no-input --clear
+
+cp -r /app/collected_static/. /backend_static/static/
+
+gunicorn --bind 0:8080 foodgram.wsgi

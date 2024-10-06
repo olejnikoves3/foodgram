97b1d561cc5cf13e728268a098098f24b073c84d
Oleinikov <j-n-d@yandex.ru>
2024-10-06 21:38:19 +0300

    push

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input --clear
 
 cp -r /app/collected_static/. /backend_static/static/
 
-gunicorn --bind 0:8080 foodgram_backend.wsgi
+gunicorn --bind 0:8000 foodgram_backend.wsgi

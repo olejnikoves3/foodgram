3aca2c687f268ff1433a1723f54231ac39faa872
Oleinikov <j-n-d@yandex.ru>
2024-10-18 00:12:04 +0300

    fix pdf, add import

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,26 +1,42 @@
 from io import BytesIO
+import os
 
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
+from django.conf import settings
 from django.http import HttpResponse
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+
+
+FONT_PATH = os.path.join(settings.BASE_DIR,
+                         'fonts/dajavu_sans/DejaVuSans.ttf')
 
 
 def generate_pdf(ingredients_summary):
     buffer = BytesIO()
     pdf = canvas.Canvas(buffer, pagesize=A4)
     width, height = A4
-    pdf.setTitle('Список покупок')
-    pdf.setFont('Times-Roman', 16)
-    pdf.drawString(100, height - 50, 'Список ингредиентов для покупок')
-    pdf.setFont('Times-Roman', 12)
+    title = 'Список ингредиентов для покупок'
+    pdf.setTitle(title)
+    pdfmetrics.registerFont(TTFont('DejaVuSans', FONT_PATH))
+    pdf.setFont('DejaVuSans', 16)
+    title_width = pdf.stringWidth(title, 'DejaVuSans', 16)
+    x_position = (width - title_width) / 2
+    pdf.drawString(x_position, height - 50, title)
+    pdf.setFont('DejaVuSans', 12)
     y_position = height - 100
+    counter = 1
     for ingredient in ingredients_summary:
         amount = ingredients_summary[ingredient]['amount']
         mes_unit = ingredients_summary[ingredient]['mes_unit']
-        pdf.drawString(100, y_position, f'{ingredient}: {amount} {mes_unit}')
+        pdf.drawString(100, y_position,
+                       f'{counter}. {ingredient}: {amount} {mes_unit}')
+        counter += 1
         y_position -= 20
         if y_position < 50:
             pdf.showPage()
+            pdf.setFont('DejaVuSans', 12)
             y_position = height - 50
     pdf.save()
     buffer.seek(0)
---
+++ backend/recipes/managment/commands/run_import.py
@@ -0,0 +1,22 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def ingredients(self, csv_reader):
+        if not Ingredient.objects.exists():
+            ingredients_to_create = [
+                Ingredient(name=row[0], measurement_unit=row[1])
+                for row in csv_reader
+            ]
+            Ingredient.objects.bulk_create(ingredients_to_create)
+
+    def handle(self, *args, **kwargs):
+        csv_file_path = '../data/ingredients.csv'
+        with open(csv_file_path, 'r', encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            self.ingredients(csv_reader)

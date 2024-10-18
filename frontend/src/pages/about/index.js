5c6b8ece8a8b7b7e13002e4a87c8b476e05aa7d0
oleinikoves <email.com>
2024-10-18 21:56:47 +0300

    add pages

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/olejnikoves3" className={styles.textLink}>Олейников Евгений</a>
             </p>
           </div>
         </aside>

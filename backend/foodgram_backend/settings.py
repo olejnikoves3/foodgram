1034d9b59cf8b5c5cbadb9f33977c4269e09b53a
Oleinikov <j-n-d@yandex.ru>
2024-10-30 21:36:33 +0300

    fix pagination

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.paginators.CustomPagination',
     'PAGE_SIZE': PAGINATION_PAGE_NUMBER
 }
 

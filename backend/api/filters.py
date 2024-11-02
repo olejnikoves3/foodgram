4d509d200d954d4fdf22cff922b2d838a12a4492
Oleinikov <j-n-d@yandex.ru>
2024-11-02 14:50:28 +0300

    fix pep

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,6 @@
-from django_filters.rest_framework import (BooleanFilter, CharFilter,
-                                           FilterSet, AllValuesMultipleFilter)
+from django_filters.rest_framework import (
+    AllValuesMultipleFilter, BooleanFilter, CharFilter, FilterSet
+)
 
 from recipes.models import Ingredient, Recipe
 
